02b1ef18af78526bf15dc213321d0bf3286f8d29
Elisha <64223532+logicshaper19@users.noreply.github.com>
2024-12-16 19:01:56 +0100

    fix: update select imports to use explicit relative path

--- src/components/data/MyData.tsx
+++ src/components/data/MyData.tsx
@@ -29,7 +29,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "../ui/select";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
